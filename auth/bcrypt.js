d5295231fad4ef0c14c833677fb931689e003a9a
Emerson Nascimento <106358298+nascimentoemerson@users.noreply.github.com>
2022-08-29 16:50:35 -0300

    feat: implemented bcrypt helper

--- auth/bcrypt.js
+++ auth/bcrypt.js
@@ -1,8 +1,8 @@
 import * as bcrypt from "bcrypt";
 
 export class BcryptHelper {
-  async hashGenerator(password) {
-    const hashedPassword = await bcrypt.hash(password, 10, (err, hash) => {
+  hashGenerator(password) {
+    const hash = bcrypt.hashSync(password, 10, (err, hash) => {
       if (err) {
         console.log(err);
         throw new Error("Error in hash");
@@ -10,9 +10,11 @@ export class BcryptHelper {
 
       return hash;
     });
-    console.log("hash: ", hashedPassword);
-    return hashedPassword;
+
+    return hash;
   }
 
-  comparePassword(password, hash) {}
+  comparePassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  }
 }
